82364e24366c2e540544bcb3a886c2f2ebd77824
Judy Li <xu3m4u6@gmail.com>
2020-02-07 18:44:07 +0800

    pdf watermarker

---
+++ PDF_watermarker/Watermarker.py
@@ -0,0 +1,17 @@
+import PyPDF2
+import sys
+
+input_file = sys.argv[1]
+marker_file = sys.argv[2]
+
+template = PyPDF2.PdfFileReader(open(input_file, 'rb'))
+watermark = PyPDF2.PdfFileReader(open(marker_file, 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+	page = template.getPage(i)
+	page.mergePage(watermark.getPage(0))
+	output.addPage(page)
+	
+with open('watermarked.pdf', 'wb') as new_file:
+	output.write(new_file)
